09da7d00a542387733bbd6d07aa5b27cad77732a
jahidRatul <jahidratul@gmail.com>
2019-08-19 02:12:30 +0600

    getting logged in users profile

--- models/Profile.js
+++ models/Profile.js
@@ -31,7 +31,7 @@ const ProfileSchema = new mongoose.Schema({
     {
       title: {
         type: String,
-        required
+        required: true
       },
       company: {
         type: String,
